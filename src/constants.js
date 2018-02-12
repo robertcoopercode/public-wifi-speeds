4843bf4d45d228a9eab6c89828964ad48591fba3
Robert Cooper <robert.cooper.qu@gmail.com>
2018-02-11 20:03:41 -0500

    Add media query support

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,20 @@
+import { css } from "styled-components"
+
 // Media queries for styled components
-export const SIZES = {
-    large: "992px",
-    medium: "700px",
-    small: "376px",
+const SIZES = {
+    large: "992",
+    medium: "700",
+    small: "376",
 }
+
+// Iterate through the sizes and create a media template
+// This allows to the use of media queries using globally set sizes
+// i.e. ${media.medium`background: blue;`};
+export const media = Object.keys(SIZES).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+        @media (max-width: ${SIZES[label] / 16}em) {
+            ${css(...args)};
+        }
+    `
+    return acc
+}, {})
